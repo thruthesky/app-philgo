3271104514ff875d46f3b71818aea4041c42bdef
thruthesky <thruthesky@gmail.com>
2015-12-05 15:31:00 +0800

    work for cache

--- js/mobile.js
+++ js/mobile.js
@@ -1,14 +1,30 @@
+var current_page_name = null;
 var url_server_ajax = url_server + 'ajax/';
 var url_server_ajax_page = url_server_ajax + 'page/';
+//var url_server_ajax_widget = url_server_ajax + 'widget/';
+//var list_widget = [ 'front_banner', 'front_text', 'forum_text', 'life_banner', 'life_text' ];
+var list_pages = ['front', 'forum', 'life'];
 $(function(){
+    setCurrentPage('front');
     initApp();
     downloadPages();
 });
 
-
+function setCurrentPage(page) {
+    current_page_name = page;
+}
+function getCurrentPage() {
+    return current_page_name;
+}
 function content() {
     return $('#content');
 }
+
+/*
+function widget(name) {
+    return $(".widget ." + name);
+}
+*/
 function header() {
     return $('header');
 }
@@ -20,11 +36,15 @@ function slidemenu() {
 }
 
 
-
-function initContent() {
+function initContent(page) {
+    /*
     ajax_load('page/content.html', function(re){
         content().html(re);
     });
+    */
+
+    if ( page ) setCurrentPage(page);
+    content().html( db.get( getCurrentPage() ) );
 }
 
 function initHeader() {
@@ -46,22 +66,29 @@ function initSlideMenu() {
 }
 
 function initApp() {
+
+    initEventHandlers();
     initHeader();
     initContent();
     initFooter();
-    initEvents();
     initSlideMenu();
 }
 
 
 
-function initEvents() {
+function initEventHandlers() {
 
     $("body").on('click', '.slide-menu-button', function(){
         $("#slide-menu").animate({
             width: "toggle"
         });
     });
+
+    $('body').on('click', 'header [page]', function() {
+        var $this = $(this);
+        var page = $this.attr('page');
+        initContent(page);
+    })
 }
 
 
@@ -72,7 +99,36 @@ function initEvents() {
  */
 function downloadPages() {
 
-    ajax_load(url_server_ajax_page + 'front', function(re) {
+    for( i in list_pages ) {
+        downloadPageAndRender(list_pages[i]);
+    }
+}
+function downloadPageAndRender(page_name) {
+    page_name += "?dummy=" + new Date().getTime();
+    ajax_load(url_server_ajax_page + page_name, function(re) {
         console.log(re);
+        if ( re.html ) {
+            db.set(page_name, re.html);
+        }
+        if ( getCurrentPage() == page_name ) {
+            content().html(re.html);
+        }
     });
 }
+/*
+function downloadWidgets() {
+
+    for( i in list_widget ) {
+        downloadWidgetAndRender(list_widget[i]);
+    }
+}
+function downloadWidgetAndRender($widget_name) {
+    ajax_load(url_server_ajax_widget + $widget_name, function(re) {
+        //console.log(re);
+        if ( re.html ) {
+            db.set($widget_name, re.html);
+        }
+        widget( $widget_name).html(re.html);
+    });
+}
+*/
